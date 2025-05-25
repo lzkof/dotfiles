beede6c26fd8632059012de33883b7602082c86a
Lorenz Kofler <lorenz@lzko.xyz>
2025-05-25 13:38:17 +0200

    update
    fix

--- nvim/lua/custom/plugins/markdown-preview.lua
+++ nvim/lua/custom/plugins/markdown-preview.lua
@@ -1,8 +1,21 @@
 return {
-    {
-        "iamcco/markdown-preview.nvim",
-        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-        ft = { "markdown" },
-        build = function() vim.fn["mkdp#util#install"]() end,
-    }
+	{
+		"iamcco/markdown-preview.nvim",
+		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+		ft = { "markdown" },
+		build = function()
+			vim.fn["mkdp#util#install"]()
+		end,
+	},
+
+	{
+		"toppair/peek.nvim",
+		event = { "VeryLazy" },
+		build = "deno task --quiet build:fast",
+		config = function()
+			require("peek").setup()
+			vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
+			vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
+		end,
+	},
 }
--- zsh/.zshenv
+++ zsh/.zshenv
@@ -1,5 +1,8 @@
 export ZDOTDIR=$HOME/.config/zsh
 source $ZDOTDIR/aliases
 source $ZDOTDIR/scripts
+source $ZDOTDIR/exports
 source $ZDOTDIR/keybindings
+export EDITOR=nvim
+
 
